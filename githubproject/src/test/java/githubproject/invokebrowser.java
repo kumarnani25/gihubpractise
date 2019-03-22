150b0330a98f6b55a848ef9a12b3134adeec39ed
kumarnani25 <kumar.nani25@gmail.com>
2019-03-22 08:51:28 +0530

    comit html driver

--- githubproject/src/test/java/githubproject/invokebrowser.java
+++ githubproject/src/test/java/githubproject/invokebrowser.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.annotations.Test;
 
 public class invokebrowser {
@@ -12,14 +13,16 @@ public class invokebrowser {
 	@Test
 	public void test()
 	{
-		System.setProperty("webdriver.chrome.driver", ".//src/main///resources//chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+	//	System.setProperty("webdriver.chrome.driver", ".//src/main///resources//chromedriver.exe");
+		//WebDriver driver=new ChromeDriver();
+		WebDriver driver=new HtmlUnitDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
 		driver.findElement(By.id("btnLogin")).click();
-		driver.findElement(By.id("btnLogin")).click();
+	String title=driver.getTitle();
+	System.out.println(title);
 	}
 	
 
